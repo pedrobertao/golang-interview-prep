724405286371da27d2c4711ade398a77e309bdf1
Pedro Bertao <pedrobertao11@hotmail.com>
2025-06-26 00:08:05 -0300

    :beers: Add bubblesort example

--- codes/bubbleSort/bubbleSort.go
+++ codes/bubbleSort/bubbleSort.go
@@ -3,7 +3,6 @@ package bubbleSort
 import (
 	"fmt"
 	"math/rand"
-	"sort"
 	"time"
 )
 
@@ -11,13 +10,11 @@ func GenerateData(numOfInt int) []int {
 	src := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(src)
 
-	// Build sequential slice
 	data := make([]int, numOfInt)
 	for i := range data {
 		data[i] = i
 	}
 
-	// Shuffle with local rand
 	r.Shuffle(numOfInt, func(i, j int) {
 		data[i], data[j] = data[j], data[i]
 	})
@@ -26,22 +23,27 @@ func GenerateData(numOfInt int) []int {
 }
 
 func Sort(list []int) {
+	fmt.Println("> Before: ", list)
 	start := time.Now()
-	sorted := false
-	for !sorted {
-		for i, v := range list {
-			if i >= 0 && i < len(list)-1 {
-				if v > list[i+1] {
-					temp := list[i+1]
-					list[i+1] = v
-					list[i] = temp
-				}
+	for {
+		swapped := false // Track if any swaps occurred
+		for i := 0; i < len(list)-1; i++ {
+			if list[i] > list[i+1] {
+				temp := list[i+1]
+				list[i+1] = list[i]
+				list[i] = temp
+				swapped = true
 			}
 		}
-		sorted = sort.SliceIsSorted(list, func(i, j int) bool {
-			return list[j] > list[i]
-		})
+		if !swapped {
+			break
+		}
 	}
 	endTime := time.Since(start)
 	fmt.Printf("Sorted the list with %d in %fs.\n", len(list), endTime.Seconds())
+	fmt.Println("> After: ", list)
+}
+
+func Example() {
+	Sort(GenerateData(20))
 }
--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"gopreinterview/codes/bubbleSort"
 )
 
-func example_bubbleSort() {
-	// Simple
-	fmt.Println("====================Bubble Sort ")
-	// Small Data Set
-	bubbleSort.Sort(bubbleSort.GenerateData(100))
-	// Massive Data Set
-	bubbleSort.Sort(bubbleSort.GenerateData(100_000))
-	fmt.Println("====================Bubble Sort")
-}
-
 func main() {
-	fmt.Println("Here you will find several example of codes for interviews")
-	// example_bubbleSort()
+	bubbleSort.Example()
 }
